e4461bee7595770c14947ae23df9bb53642268b0
Jessica GUALTIERI <jessicagualtieri@hotmail.fr>
2024-01-30 10:39:23 +0100

    TP - fonctions et correction anciens TP

---
+++ TP-fonctions/TP-fonctions/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        // Niveau 1
+        // Consigne : créer une fonction de calcul de l'aire d'un rectangle
+        public static int AireRectangle(int largeur, int longueur)
+        {
+            return largeur * longueur;
+        }
+
+        // Niveau 2
+        // Consigne : créer une fonction qui affichera X fois quelque chose
+        public static void AfficherXfois(int nombreDeFois, string contenu)
+        {
+            for (int i = 0; i < nombreDeFois; i++)
+            {
+                Console.WriteLine(contenu);
+            }
+        }
+
+        // Niveau 3
+        // Consigne : créer une fonction
+        public static int CalculExposant(int nombreBase, int nombreExposant)
+        {
+            int result = nombreBase;
+            for (int i = 1; i < nombreExposant; i++) // Démarre à 1 car on a déjà fait 1x en assignant nombreBase à result
+            {
+                result *= nombreBase;
+            }
+            return result;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            // Niveau 1
+            int rectangle = AireRectangle(2, 5);
+            Console.WriteLine(rectangle);
+
+            // Niveau 2
+            AfficherXfois(5, "Hodor");
+
+            // Niveau 3
+            int calcul2Puissance10 = CalculExposant(2, 10);
+            Console.WriteLine(calcul2Puissance10);
+            int calcul5Puissance3 = CalculExposant(5, 3);
+            Console.WriteLine(calcul5Puissance3);
+        }
+    }
+}
+
--- TP-juste-prix/TP-juste-prix/Program.cs
+++ TP-juste-prix/TP-juste-prix/Program.cs
@@ -9,12 +9,12 @@ namespace MyApp // Note: actual namespace depends on the project name.
             // VARIABLES :
             int chiffreUtilisateur;
             int chiffreMystere;
-            var rand = new Random();
-            int nbTentatives = 1;
+            Random random = new Random();
+            int nombreTentatives = 1;
             string plurielTentative = "";
-            bool restart = false;
-            string userRestart;
-            int maxChiffreMystere;
+            bool recommencer = false;
+            string reponseUtilisateurRecommencer;
+            int maximumChiffreMystere;
 
             // LE JEU :
             do
@@ -31,64 +31,64 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 }
 
                 // RÉINITIALISATION DES VARIABLES POUR REPARTIR SUR UNE NOUVELLE PARTIE
-                restart = false;
-                nbTentatives = 1;
+                recommencer = false;
+                nombreTentatives = 1;
                 // RANDOM SELON LE NIVEAU DE DIFFICULTÉS
                 if (difficulte == "facile")
                 {
-                    chiffreMystere = rand.Next(1, 101);
-                    maxChiffreMystere = 100;
+                    chiffreMystere = random.Next(1, 101);
+                    maximumChiffreMystere = 100;
                 }
                 else if (difficulte == "moyen")
                 {
-                    chiffreMystere = rand.Next(1, 1001);
-                    maxChiffreMystere = 1000;
+                    chiffreMystere = random.Next(1, 1001);
+                    maximumChiffreMystere = 1000;
                 }
                 else
                 {
-                    chiffreMystere = rand.Next(1, 10001);
-                    maxChiffreMystere = 10000;
+                    chiffreMystere = random.Next(1, 10001);
+                    maximumChiffreMystere = 10000;
                 }
                 Console.WriteLine(chiffreMystere);
                 // BOUCLE DE JEU :
                 do
                 {
-                    Console.WriteLine("\nEntrez un nombre entre 1 et " + maxChiffreMystere + ":");
+                    Console.WriteLine("\nEntrez un nombre entre 1 et " + maximumChiffreMystere + ":");
                     chiffreUtilisateur = int.Parse(Console.ReadLine());
-                    if (chiffreUtilisateur < 1 || chiffreUtilisateur > maxChiffreMystere)
+                    if (chiffreUtilisateur < 1 || chiffreUtilisateur > maximumChiffreMystere)
                     {
                         Console.WriteLine("Erreur, le chiffre est en dehors de l'intervalle recherchée.");
                     }
                     else if (chiffreUtilisateur < chiffreMystere)
                     {
                         Console.WriteLine("C'est + !");
-                        nbTentatives++;
+                        nombreTentatives++;
                     }
                     else if (chiffreUtilisateur > chiffreMystere)
                     {
                         Console.WriteLine("C'est - !");
-                        nbTentatives++;
+                        nombreTentatives++;
                     }
                 } while (chiffreUtilisateur != chiffreMystere);
 
                 // POUR ACCORDER 'coup' AU PLURIEL SI BESOIN :
-                if (nbTentatives > 1)
+                if (nombreTentatives > 1)
                     plurielTentative = "s";
 
                 // AFFICHAGE DE VICTOIRE :
-                Console.WriteLine("\nBRAVO : le chiffre mystère était bien : " + chiffreMystere + ", trouvé en " + nbTentatives + " coup" + plurielTentative + " !");
+                Console.WriteLine("\nBRAVO : le chiffre mystère était bien : " + chiffreMystere + ", trouvé en " + nombreTentatives + " coup" + plurielTentative + " !");
 
                 // RECOMMENCER ?
                 Console.WriteLine("Voulez-vous recommencer (oui/non) ?");
-                userRestart = Console.ReadLine();
+                reponseUtilisateurRecommencer = Console.ReadLine();
                 // CONVERSION DE LA RÉPONSE EN BOOLÉEN POUR RELANCER LE JEU (TRUE) OU LE STOPPER (FALSE)
-                if (userRestart == "oui")
-                    restart = true;
-                else if (userRestart == "non")
-                    restart = false;
+                if (reponseUtilisateurRecommencer == "oui")
+                    recommencer = true;
+                else if (reponseUtilisateurRecommencer == "non")
+                    recommencer = false;
 
                 // TANT QU'ON VEUT RECOMMENCER, ON BOUCLE
-            } while (restart == true);
+            } while (recommencer == true);
             
         }
     }
