9e337455fe04e9bb0a1e89e0ba201f39c151c9b4
Jessica GUALTIERI <jessicagualtieri@hotmail.fr>
2024-02-12 15:43:11 +0100

    Exercices sur les tests unitaires

---
+++ TP_UnitTests_FizzBuzzProject/FizzBuzzProject.UnitTests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿namespace FizzBuzzProject.UnitTests;
+
+[TestClass]
+public class UnitTest1
+{
+   
+
+    [TestMethod]
+    public void GetOutput_UsingModulos3_ReturnFizz()
+    {
+        // Act
+        string result = FizzBuzz.GetOutput(3);
+
+        // Assert
+        Assert.AreEqual(result, "Fizz");
+    }
+
+    [TestMethod]
+    public void GetOutput_UsingModulos5_ReturnFizz()
+    {
+        // Act
+        string result = FizzBuzz.GetOutput(5);
+
+        // Assert
+        Assert.AreEqual(result, "Buzz");
+    }
+
+    [TestMethod]
+    public void GetOutput_UsingModulos3And5_ReturnBuzz()
+    {
+        // Act
+        string result = FizzBuzz.GetOutput(15);
+
+        // Assert
+        Assert.AreEqual(result, "FizzBuzz");
+    }
+
+    [TestMethod]
+    public void GetOutput_UsingNoModulos_ReturnNumber()
+    {
+        // Act
+        string result = FizzBuzz.GetOutput(4);
+
+        // Assert
+        Assert.AreEqual(result, "4");
+    }
+}
---
+++ TP_UnitTests_FizzBuzzProject/FizzBuzzProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FizzBuzzProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
