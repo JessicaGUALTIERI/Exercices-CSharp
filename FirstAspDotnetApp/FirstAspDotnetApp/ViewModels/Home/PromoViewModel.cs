7892876e5b4604d21ca49374928b3ee0b77dd5c6
Jessica GUALTIERI <jessicagualtieri@hotmail.fr>
2024-03-07 11:53:10 +0100

    🚀 Premiers tests sur ASP .NET

---
+++ FirstAspDotnetApp/FirstAspDotnetApp/ViewModels/Home/PromoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace FirstAspDotnetApp.ViewModels.Home
+{
+	public class PromoViewModel
+	{
+		public List<string> Promos { get; set; }
+		public PromoViewModel()
+		{
+            Promos = new List<string>()
+            {
+                "Dev Web 2",
+                "Dev Web 1",
+                "CDA C#",
+                "MSRC1 23/25",
+                "M2I1 23/25",
+                "CDA JAVA",
+                "TSSR",
+                "BSRC2 22/24"
+            };
+        }
+    }
+}
+
