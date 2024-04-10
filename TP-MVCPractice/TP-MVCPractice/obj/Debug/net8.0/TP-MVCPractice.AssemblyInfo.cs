b059864c8ddb4973f26e74646340707319b779e4
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-10 19:05:19 +0200

    Add CRUD for categories

--- TP-MVCPractice/TP-MVCPractice/obj/Debug/net8.0/TP-MVCPractice.AssemblyInfo.cs
+++ TP-MVCPractice/TP-MVCPractice/obj/Debug/net8.0/TP-MVCPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-MVCPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e7c0686c0ac952ff4d7e0a8015c2f3f2176f370")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f341a7380a7b9604dc7ccb5e2347d34dcd7c06a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-MVCPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-MVCPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
