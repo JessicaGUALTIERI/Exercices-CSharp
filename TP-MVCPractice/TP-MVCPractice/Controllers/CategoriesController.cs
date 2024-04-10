2820d896f51c9345d5a673c1a4a5eeb404c58e2e
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-10 14:37:57 +0200

    Practicing MVC Model through a tutorial

---
+++ TP-MVCPractice/TP-MVCPractice/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP_MVCPractice.Controllers;
+
+public class CategoriesController : Controller
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+
+	public IActionResult Edit(int? id)
+	{
+		if (id.HasValue)
+			return new ContentResult { Content = id.ToString() };
+		else
+			return new ContentResult { Content = "No content" };
+	}
+}
+
