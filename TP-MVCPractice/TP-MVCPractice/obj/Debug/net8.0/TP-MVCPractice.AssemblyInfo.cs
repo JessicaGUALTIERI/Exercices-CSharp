aecdf80cb9552e68675c0562f52ccd94d34f9479
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-15 13:25:06 +0200

    Updated the add form

--- TP-MVCPractice/TP-MVCPractice/obj/Debug/net8.0/TP-MVCPractice.AssemblyInfo.cs
+++ TP-MVCPractice/TP-MVCPractice/obj/Debug/net8.0/TP-MVCPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-MVCPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75983aecd48a31f795a5c33a94a0c4a423aec2cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb04c5974c6f970583e5794ac41ff1c5e9cecef2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-MVCPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-MVCPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
