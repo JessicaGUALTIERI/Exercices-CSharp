371544a8dba09b4ba676ba17208718ad7eecd293
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-16 11:52:04 +0200

    Working on API datas

--- TP-File-WorkingOnDatas/TP-File-WorkingOnDatas/Program.cs
+++ TP-File-WorkingOnDatas/TP-File-WorkingOnDatas/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace TP_File_WorkingOnDatas;
+﻿using System.Xml;
+using static System.Net.WebRequestMethods;
+
+namespace TP_File_WorkingOnDatas;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("-------------- PART ONE --------------");
         string fileName = "ScoresGOT.csv";
         string path = AppDomain.CurrentDomain.BaseDirectory + "../../../datas/" + fileName;
         StreamReader sr = new StreamReader(path);
@@ -33,6 +37,76 @@ class Program
         averageScore = Math.Round(averageScore, 2);
         Console.WriteLine("[Stats for " + numberOfGames + " games] Average success rate : " + averageSuccessRate + " | Average score : " + averageScore);
         sr.Close();
+
+        Console.WriteLine("\n-------------- PART TWO --------------");
+        string xmlFileName = "books.xml";
+        string xmlPath = AppDomain.CurrentDomain.BaseDirectory + "../../../datas/" + xmlFileName;
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(xmlPath);
+
+        foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+        {
+            XmlNode firstChildNode = node.ChildNodes[0];
+            Console.WriteLine(firstChildNode.InnerText);
+        }
+
+        Console.WriteLine("\n-------------- PART THREE --------------");
+        string urlApi = "https://api.geekdo.com/xmlapi2/";
+        XmlDocument xmlDocApi = new XmlDocument();
+
+        Console.Write("[Search by id] - Please enter the id :\n\t\t>>> ");
+        string idSearched = "thing?id=" + Console.ReadLine();
+        string completeUrlForIdSearch = urlApi + idSearched;
+        xmlDocApi.Load(completeUrlForIdSearch);
+        XmlNodeList xmlNodeListForIdSearch = xmlDocApi.GetElementsByTagName("name");
+        Console.WriteLine(xmlNodeListForIdSearch[0].Attributes["value"].InnerText);
+
+        Console.WriteLine("--------------------------------------------------------------------------------------");
+
+        Console.Write("[Search by string] - Please enter the string :\n\t\t>>> ");
+        string stringSearched = "search?query=" + Console.ReadLine();
+        string completeUrlForStringSearch = urlApi + stringSearched;
+        int countOfResults = 0;
+        xmlDocApi.Load(completeUrlForStringSearch);
+        foreach (XmlNode node in xmlDocApi.DocumentElement.ChildNodes)
+        {
+            XmlNode xmlNode = node.ChildNodes[0];
+            Console.WriteLine(xmlNode.Attributes["value"].InnerText);
+            countOfResults++;
+        }
+        DisplayCountOfResults(countOfResults);
+
+        Console.WriteLine("--------------------------------------------------------------------------------------");
+
+        Console.Write("[Search by string with details displayed] - Please enter the string :\n\t\t>>> ");
+        stringSearched = "search?query=" + Console.ReadLine();
+        string completeUrlForStringSearchAndDetails = urlApi + stringSearched;
+        countOfResults = 0;
+        xmlDocApi.Load(completeUrlForStringSearchAndDetails);
+        foreach (XmlNode node in xmlDocApi.DocumentElement.ChildNodes)
+        {
+            XmlNode xmlNode = node.ChildNodes[0];
+            string name = xmlNode.Attributes["value"].InnerText;
+            idSearched = "thing?id=" + node.Attributes["id"].InnerText;
+            completeUrlForIdSearch = urlApi + idSearched;
+            xmlDocApi.Load(completeUrlForIdSearch);
+            XmlNodeList xmlNodeListDescription = xmlDocApi.GetElementsByTagName("description");
+            string description = xmlNodeListDescription[0].InnerText;
+            Console.WriteLine("NAME : " + name + "\nDESCRIPTION :\n" + description + "\n");
+            countOfResults++;
+        }
+        DisplayCountOfResults(countOfResults);
+
+        Console.ReadLine();
+    }
+
+    public static void DisplayCountOfResults(int countOfResults)
+    {
+        if (countOfResults > 0)
+            Console.WriteLine("[" + countOfResults + " results]");
+        else
+            Console.WriteLine("No result.");
     }
 }
+
 
--- TP-File-WorkingOnDatas/TP-File-WorkingOnDatas/obj/Debug/net8.0/TP-File-WorkingOnDatas.AssemblyInfo.cs
+++ TP-File-WorkingOnDatas/TP-File-WorkingOnDatas/obj/Debug/net8.0/TP-File-WorkingOnDatas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-File-WorkingOnDatas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b059864c8ddb4973f26e74646340707319b779e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ac270d35de31a781ee585c60c885578a54642d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-File-WorkingOnDatas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-File-WorkingOnDatas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
