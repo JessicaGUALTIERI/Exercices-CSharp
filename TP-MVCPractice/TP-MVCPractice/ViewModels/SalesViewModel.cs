9983fb317e6ceeeac67a4d82e8555284eb2cb0eb
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-16 11:53:19 +0200

    Added SalesViewModel file

---
+++ TP-MVCPractice/TP-MVCPractice/ViewModels/SalesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TP_MVCPractice.ViewModels
+{
+	public class SalesViewModel
+	{
+		public SalesViewModel()
+		{
+		}
+	}
+}
+
--- TP-MVCPractice/TP-MVCPractice/obj/Debug/net8.0/TP-MVCPractice.AssemblyInfo.cs
+++ TP-MVCPractice/TP-MVCPractice/obj/Debug/net8.0/TP-MVCPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-MVCPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d71cce5e66b9117fc9ef78c517573fde25d097")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+371544a8dba09b4ba676ba17208718ad7eecd293")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-MVCPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-MVCPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
