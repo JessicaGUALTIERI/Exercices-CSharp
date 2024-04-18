94a16434a0d54204fd6daadda616a28af5232c85
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-18 11:36:46 +0200

    Added transactionsRepository

--- TP-MVCPractice/TP-MVCPractice/Models/Transaction.cs
+++ TP-MVCPractice/TP-MVCPractice/Models/Transaction.cs
@@ -3,13 +3,13 @@ namespace TP_MVCPractice.Models
 {
 	public class Transaction
 	{
-		public int Id { get; set; }
+		public int TransactionId { get; set; }
 		public DateTime TimeStamp { get; set; }
 		public int ProductId { get; set; }
 		public string ProductName { get; set; } = "";
 		public double Price { get; set; }
-		public int QuantityBefore { get; set; }
-		public int QuantitySold { get; set; }
+		public int BeforeQty { get; set; }
+		public int SoldQty { get; set; }
 		public string CashierName { get; set; } = "";
 	}
 }
