3af634422ef32b43aa58f95795338909d90c4f53
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-04-24 10:05:32 +0200

    Exam : analyze a csv file to create a new one with the needed datas

---
+++ Exam-BearsData/Exam-BearsData/Program.cs
@@ -0,0 +1,36 @@
+﻿// Source : https://www.gbif.org/ 
+namespace Exam_BearsData;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fileDatasName = "Bear_Datas.csv";
+        string pathFileDatas = AppDomain.CurrentDomain.BaseDirectory + "../../../datas/" + fileDatasName;
+        StreamReader streamReader = new StreamReader(pathFileDatas);
+        string year = "";
+        string gbifID = "";
+        string line = "";
+        string[] values;
+
+        string fileResultName = "Bear_Result.csv";
+        string pathFileResult = AppDomain.CurrentDomain.BaseDirectory + "../../../datas/" + fileResultName;
+        StreamWriter streamWriter = new StreamWriter(pathFileResult);
+        streamWriter.Write("YEAR;GBIFID;\n");
+        while (!streamReader.EndOfStream)
+        {
+            line = streamReader.ReadLine();
+            values = line.Split(';');
+            if (values[35] == "HUMAN_OBSERVATION")
+            {
+                year = values[32];
+                gbifID = values[0];
+                string result = year + ";" + gbifID + ";\n";
+                streamWriter.Write(result);
+            }
+        }
+        streamReader.Close();
+        streamWriter.Close();
+    }
+}
+
